9ada514f256145f58ed82aef423684704d5a7eb8
CheeSengg <e0309953@u.nus.edu>
2022-02-16 20:03:57 +0800

    Added sort merge

--- src/simpledb/index/query/IndexJoinTest.java
+++ src/simpledb/index/query/IndexJoinTest.java
@@ -14,7 +14,7 @@ import simpledb.index.planner.IndexJoinPlan;
 
 public class IndexJoinTest {
 	public static void main(String[] args) {
-		SimpleDB db = new SimpleDB("studentdb");
+		SimpleDB db = new SimpleDB("studentdb2");
       MetadataMgr mdm = db.mdMgr();
       Transaction tx = db.newTx();
 
--- src/simpledb/opt/TablePlanner.java
+++ src/simpledb/opt/TablePlanner.java
@@ -6,6 +6,7 @@ import simpledb.record.*;
 import simpledb.query.*;
 import simpledb.metadata.*;
 import simpledb.index.planner.*;
+import simpledb.materialize.MergeJoinPlan;
 import simpledb.multibuffer.MultibufferProductPlan;
 import simpledb.plan.*;
 
@@ -64,7 +65,7 @@ class TablePlanner {
       Predicate joinpred = mypred.joinSubPred(myschema, currsch);
       if (joinpred == null)
          return null;
-      Plan p = makeIndexJoin(current, currsch);
+      Plan p = makeMergeJoin(current, currsch);
       if (p == null)
          p = makeProductJoin(current, currsch);
       return p;
@@ -109,6 +110,18 @@ class TablePlanner {
    private Plan makeProductJoin(Plan current, Schema currsch) {
       Plan p = makeProductPlan(current);
       return addJoinPred(p, currsch);
+   }
+
+   private Plan makeMergeJoin(Plan current, Schema currsch) {
+      for (String fldname : this.myschema.fields()) {
+         String fldname2 = mypred.equatesWithField(fldname);
+         if (fldname != null && currsch.hasField(fldname2)) {
+            Plan p = new MergeJoinPlan(tx, myplan, current, fldname, fldname2);
+            p = addSelectPred(p);
+            return addJoinPred(p, currsch);
+         }
+      }
+      return null;
    }
    
    private Plan addSelectPred(Plan p) {
